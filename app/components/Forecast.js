d809d50c6ce4db38f8bac73385d385a31202e70d
Barbara Verburg <barb@zenzillo.com>
2019-03-20 15:57:12 -0400

    Setup weather api connection

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,6 +1,12 @@
 var React = require('react');
+var api = require('../utils/api');
+var queryString = require('query-string');
 
 class Forecast extends React.Component {
+	componentDidMount() {
+		const values = queryString.parse(this.props.location.search);
+		api.fetch5DayWeather(values.city);
+	}
 	render() {
 		return (
 		    <div className='home-container'>
---
+++ app/utils/api.js
@@ -0,0 +1,32 @@
+var axios = require('axios');
+
+// current weather
+//https://api.openweathermap.org/data/2.5/weather?q=CITY-NAME&type=accurate&APPID=YOUR-API-KEY
+
+// 5 day weather
+//https://api.openweathermap.org/data/2.5/forecast/daily?q=CITY-NAME&type=accurate&APPID=YOUR-API-KEY&cnt=5
+
+var api_key = '502406f67bc5c08321127e72dffabb39';
+var params = '&type=accurate&APPID=' + api_key;
+
+function handleError (error) {
+	console.warn(error);
+	return null;
+}
+
+module.exports =  {
+	fetch5DayWeather: function (city) {
+		return axios.get('https://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + params + '&cnt=5')
+			.then(function (weather) {
+				return weather.data;
+			})
+			.catch(handleError)
+	},
+	fetchCurrentWeather: function (city) {
+		return axios.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + params)
+			.then(function (weather) {
+				return weather.data;
+			})
+			.catch(handleError)
+	}
+}
