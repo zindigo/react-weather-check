6b844f516b0ce91a09550cf98949551ffcfe5a02
Barbara Verburg <barb@zenzillo.com>
2019-03-20 17:01:10 -0400

    Add loading message on forecast page

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -3,16 +3,39 @@ var api = require('../utils/api');
 var queryString = require('query-string');
 
 class Forecast extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: false
+		}
+
+		this.makeRequest = this.makeRequest.bind(this);
+	}
 	componentDidMount() {
 		const values = queryString.parse(this.props.location.search);
-		api.get5DayWeather(values.city);
+		this.makeRequest(values.city);
+	}
+	makeRequest(city) {
+		this.setState(function () {
+			return {
+				loading: true
+			}
+		})
+
+		api.get5DayWeather(city)
+			.then(function (result) {
+				console.log(result)
+				this.setState(function() {
+					return {
+						loading: false
+					}
+				})
+			}.bind(this))
 	}
 	render() {
-		return (
-		    <div className='home-container'>
-		    	<h1>Forecast</h1>
-		    </div>
-		)
+		    return this.state.loading === true
+		      ? <div>Loading</div>
+		      : <div>Forecast Component</div>
 	}
 }
 
