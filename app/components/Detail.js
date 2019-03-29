8cab9d4db145e44864e5096361253cacedb1d64e
Barbara Verburg <barb@zenzillo.com>
2019-03-29 13:36:20 -0400

    Add button to switch between Farenheit and Celsius

--- app/components/Detail.js
+++ app/components/Detail.js
@@ -5,11 +5,51 @@ var ReactRouter = require('react-router-dom');
 var BrowserHistory = ReactRouter.BrowserHistory;
 
 class Detail extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			noonTemp: null,
+			units: 'F' // default (C=Celsius, F=Farenheit)
+		}
+
+		this.handleClick = this.handleClick.bind(this);
+		this.findNoonTemp = this.findNoonTemp.bind(this);
+	}
+	componentDidMount() {
+		// Find default temperature (Farenheit)
+		this.setState(function () {
+			return {
+				noonTemp: this.findNoonTemp(this.state.units)
+			}
+		})
+	}
+	findNoonTemp(units) {
+
+		var temp = 0;
+		var details = this.props.location.state;
+
+		if (units == 'F') { // convert to Farenheit
+			temp = helpers.convertKtoF(details.main.temp_max).toFixed(0);
+		}
+		else if (units == 'C') { // convert to Celsius
+			temp = helpers.convertKtoC(details.main.temp_max).toFixed(0);
+		}
+
+		return temp;
+	}
+	handleClick(units) {
+		this.setState(function () {
+			return {
+				noonTemp: this.findNoonTemp(units),
+				units: units
+			}
+		})
+	}
 	render() {
 		var details = this.props.location.state;
 		var description = details.weather[0].description;
 		description = helpers.capitalize(description);
-		var temp_max = helpers.convertKtoF(details.main.temp_max).toFixed(0);
+
 		return (
 		    <div className='txt-center'>
 		    	<h1 className='header'>{details.city}</h1>
@@ -17,7 +57,10 @@ class Detail extends React.Component {
 			    	<DailyWeather day={details} />
 			    	<div className='details'>
 				    	<p>{description}</p>
-				    	<p>Noon temp: {temp_max}&deg; F</p>
+				    	<p>Noon Temperature: {this.state.noonTemp}&deg; {this.state.units}<br />
+				    	<button onClick={this.handleClick.bind(this, 'C')}>&deg; C</button>
+				    	<button onClick={this.handleClick.bind(this, 'F')}>&deg; F</button>
+				    	</p>
 				    	<p>Humidity: {details.main.humidity}%</p>
 			    	</div>
 			    </div>
